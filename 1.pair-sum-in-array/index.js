e6ca3a15be4a3029f07cfa46fceb8bedaa8894a7
siddharthasarma <sidduinevitable@gmail.com>
2019-06-12 06:49:05 +0530

    other solution

--- 1.pair-sum-in-array/index.js
+++ 1.pair-sum-in-array/index.js
@@ -3,11 +3,13 @@ Find pair with given sum in the array
 Input: [8, 7, 2, 5, 3, 1]
 sum = 10
 
-output: [[0, 2], [1. 4]]
+output: [[8, 2], [7, 3]]
 */
 
-// Approach - 1
-
+/* 
+  Approach - 1
+  time complexity: O(n^2)
+*/
 const findPairApproachOne = (arr, sum) => {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -15,6 +17,24 @@ const findPairApproachOne = (arr, sum) => {
       if (arr[i] + arr[j] === sum) {
         result.push([arr[i], arr[j]]);
       }
+    }
+  }
+  return result;
+};
+
+/* 
+  Approach - 2
+  time complexity: O(n)
+*/
+const findPairApproachTwo = (arr, sum) => {
+  const result = [];
+  const map = arr.reduce((a, b) => {
+    a[b] = b;
+    return a;
+  }, {});
+  for (let num in map) {
+    if (map.hasOwnProperty[sum - num]) {
+      result.push([num, sum - num]);
     }
   }
   return result;
